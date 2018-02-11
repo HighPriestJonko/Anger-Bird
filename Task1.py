519e542961bf7ed68e056d03e038af302ce4889b
zhangminglei <zml13856086071@163.com>
2018-02-11 23:45:50 +0800

    Finish the project1 of Bird Attack

--- Task1.py
+++ Task1.py
@@ -4,7 +4,7 @@ v = int(input('Enter a velocity in m/s:')) # With which bird was flung
 a = int(input('Enter an angle in degrees:')) # With respect to the horizontal
 d = int(input('Enter distance to structure in m:'))
 h = int(input('Enter a height in m:')) # Height of structure
-g = 9.8 # m/s^2
+g = -9.8 # m/s^2
 
 degC = math.pi / 180
 ar = a * degC  # Degrees into radians for computational purposes 
@@ -22,9 +22,12 @@ dx = horizontal_speed * t # Horizontal position
 
 v_co_speed = math.sqrt(vf_vertical * vf_vertical + horizontal_speed * horizontal_speed)
 
+degrees = math.acos(horizontal_speed / v_co_speed) * 180 / math.pi
+
 print('Did the Red Bird hit the structure? Let\'s review the results:\n')
 print('Your Red Bird reached the structure in ', t, 's')
 print('and was traveling at a velocity of ', v_co_speed, 'm/s')
+print(' at an angle of ', degrees, "below the horizontal")
 print('The Red Bird was at a height of', dy + h, 'm from the ground')
 print('when it reached your intended structure, so your Red Bird', '', 'the structure.')
 
